b980658d04ef2dceea13616495c970249adef2e8
Julián López Triñanes <a24julianlt@iessanclemente.net>
2026-01-19 19:21:28 +0100

    tema y fragmentos añadidos

---
+++ app/src/main/java/com/example/appdenotas/AjustesFragment.kt
@@ -0,0 +1,17 @@
+package com.example.appdenotas
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+class AjustesFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_ajustes, container, false)
+    }
+
+}
